2ceb2cb1cf62180a7aff7fa2a1b7394bcb847531
miloroot <milonice@hotmail.com>
2015-03-19 09:35:55 +0100

    Footer has the requiered information. Starting to edit the 'design' of the view/home.html in homeController.js

--- wp-content/themes/ngTheme/js/controllers/homeController.js
+++ wp-content/themes/ngTheme/js/controllers/homeController.js
@@ -9,17 +9,21 @@ app.controller("homeController", ["$scope", "Pages", "$sce", function($scope, Pa
   //listening for the "gotPageData" broadcast on $http success
   $scope.$on("gotPageData", function(event, data) {
     console.log("homeController on gotPageData: ", data);
+    console.log("gotPageData, the title: ", data[0].title);
 
-    /* 
+    /*
       angular protects us from "dangerous" HTML by converting it to a string
 
-      if we want to show HTML from a string in DOM 
+      if we want to show HTML from a string in DOM
       we first need to tell angular that it can be trusted.
-      
+
       this is done using the $sce service on the HTML string in JS
       and the ng-bind-html directive in the view
     */
-    $scope.trustedHtml = $sce.trustAsHtml(data[0].content);
+    var title = '<h2>' + data[0].title + '</h2>';
+    var content = data[0].content;
+
+    $scope.trustedHtml = $sce.trustAsHtml(title + content);
   });
-  
+
 }]);
