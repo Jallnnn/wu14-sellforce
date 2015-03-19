8e817a0de91abf28724c45093e8c934fa1cf6f7f
TKatra <tobiaskatra@hotmail.com>
2015-03-19 14:54:08 +0100

    ändrat postfactory.js så den kan använda sig av callbacks och även göra html-stycken säkra för angular

--- wp-content/themes/ngTheme/js/services/postfactory.js
+++ wp-content/themes/ngTheme/js/services/postfactory.js
@@ -1,8 +1,23 @@
-app.factory("Posts", ["WPRest", function(WPRest) {
+app.factory("Posts", ["WPRest", "$sce", function(WPRest, $sce) {
   var postServant = {
     get : function(postId) {
       var callUrl = postId ? "/posts/"+postId : "/posts";
-      WPRest.restCall(callUrl, "GET", {}, "gotPostData");
+      WPRest.restCall(callUrl, "GET", {}, {
+        broadcastName: "gotPostData",
+        callback: function(data) {
+          if (data.constructor.name == "Array") {
+            data.forEach(function(item) {
+              item.excerpt = $sce.trustAsHtml(item.excerpt);
+              item.content = $sce.trustAsHtml(item.content);
+            });
+          } else {
+            data.excerpt = $sce.trustAsHtml(data.excerpt);
+            data.content = $sce.trustAsHtml(data.content);
+          }
+
+          return data;
+        }
+      });
     },
     post : function(data) {
       var callUrl = "/posts";
