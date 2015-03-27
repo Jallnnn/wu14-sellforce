fbb705b1c66eb64a6f9a8d98f77ffa0c2b34e62f
TKatra <tobiaskatra@hotmail.com>
2015-03-27 16:49:28 +0100

    gjort så att headerController får tag på vår huvudmeny och lägger ut den på $scope

--- wp-content/themes/ngTheme/js/controllers/headerController.js
+++ wp-content/themes/ngTheme/js/controllers/headerController.js
@@ -1,13 +1,16 @@
 //"ngTheme" controller.
 app.controller("headerController", ["Menus", "$scope", "$location", "SITE_INFO",
-	function(Menus, $scope, $location, SITE_INFO)
+function(Menus, $scope, $location, SITE_INFO)
+{
+	console.log("headerController is alive!");
+	console.log("SITE_INFO: ", SITE_INFO);
+
+	// console.log("Menus.get(): ", Menus.get(3));
+
+	Menus.get(3);
+	$scope.$on("gotMenuLinks", function(event, data)
 	{
-		console.log("headerController is alive!");
-		console.log("SITE_INFO: ", SITE_INFO);
-
-		console.log("Menus.get(): ", Menus.get(3));
-
-		$scope.menuLinks = {};
-
-
-	}]);
+		$scope.menuLinks = data;
+		console.log("menuLinks.items: ", $scope.menuLinks.items);
+	});
+}]);
