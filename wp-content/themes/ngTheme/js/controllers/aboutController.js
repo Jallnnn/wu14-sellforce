a35ea1320e22e0cb264817757deca6deba789a52
TKatra <tobiaskatra@hotmail.com>
2015-03-30 11:27:13 +0200

    Gjort nu så att about/om oss sidan läser från wordpress och bygger upp sidan så som det ser ut i wordpress

--- wp-content/themes/ngTheme/js/controllers/aboutController.js
+++ wp-content/themes/ngTheme/js/controllers/aboutController.js
@@ -1,4 +1,18 @@
-app.controller("aboutController", ["$scope", "Pages", "$sce", function($scope, Pages, $sce) {
-  console.log("aboutController is alive!");
-
+app.controller("aboutController", ["$scope", "Pages", "$sce",
+function($scope, Pages, $sce)
+{
+	console.log("aboutController is alive!");
+	Pages.get();
+	$scope.$on("gotPageData", function(event, data)
+	{
+		console.log("pages: ", data);
+		for (var i = 0; i < data.length; i++)
+		{
+			if (data[i].slug == "om-oss")
+			{
+				var pageHtml = "<h2>" + data[i].title + "</h2>" + data[i].content;
+				$scope.trustedHtml = $sce.trustAsHtml(pageHtml);
+			}
+		}
+	});
 }]);
